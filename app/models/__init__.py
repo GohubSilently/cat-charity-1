bef3966c72d82a323319058d71b602ffad057027
Vadim <khalinvadim3@gmail.com>
2026-01-10 18:47:39 +0300

    Fix code for tests;

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,2 @@
-from .charity_project import CharityProject  # noqa: F401
-from .donation import Donation  # noqa: F401
+from app.models.charity_project import CharityProject  # noqa: F401
+from app.models.donation import Donation  # noqa: F401
