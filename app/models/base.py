c6d0515326aeeb294e517c4b8b16065a2f430048
Vadim <khalinvadim3@gmail.com>
2026-01-13 08:50:29 +0300

    Fifth Review;

--- app/models/base.py
+++ app/models/base.py
@@ -31,7 +31,7 @@ class InvestmentInformation(Base):
         self.invested_amount = 0
 
     def __repr__(self) -> str:
-        return (f'{super().__repr__()}\n'
+        return (f'{super().__repr__()}\n'  # noqa: FCS100
                 f'full_amount={self.full_amount}\n'
                 f'invested_amount={self.invested_amount}\n'
                 f'fully_invested={self.fully_invested}\n'
